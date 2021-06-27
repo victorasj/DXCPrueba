ae4bf58417406a0724e4225a0bbbf3c8a7858c27
Victor <victor.sanchezjimenez.smr@gmail.com>
2021-06-27 18:30:59 +0200

    Renombrado clases

--- app/src/main/java/com/victorasj/dxcprueba/server/PhotoTitle.kt
+++ app/src/main/java/com/victorasj/dxcprueba/server/PhotoTitle.kt
@@ -2,7 +2,7 @@ package com.victorasj.dxcprueba.server
 
 import com.google.gson.annotations.SerializedName
 
-class Title(
+data class PhotoTitle (
     @SerializedName("_content")
     val title : String
 )
