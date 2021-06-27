558638eb4b01d808f1080afb44b9126cafdc535f
Victor <victor.sanchezjimenez.smr@gmail.com>
2021-06-27 18:07:19 +0200

    Creando MainViewModel

--- app/src/main/java/com/victorasj/dxcprueba/ui/main/MainViewModel.kt
+++ app/src/main/java/com/victorasj/dxcprueba/ui/main/MainViewModel.kt
@@ -1,7 +1,38 @@
 package com.victorasj.dxcprueba.ui.main
 
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.victorasj.domain.Photo
+import com.victorasj.dxcprueba.Event
+import com.victorasj.dxcprueba.common.ScopedViewModel
+import com.victorasj.interactor.GetPhotos
+import kotlinx.coroutines.launch
 
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class MainViewModel(private val getPhotos : GetPhotos, private val tag : String) : ScopedViewModel() {
+
+    sealed class UiModel {
+        object Loading : UiModel()
+        class Content(val photos : List<Photo>) : UiModel()
+    }
+
+    private val _photos = MutableLiveData<UiModel>()
+    val photos : LiveData<UiModel>
+        get() {
+            if(_photos.value == null) refresh(tag)
+            return _photos
+        }
+
+
+    private val _navigateToPhoto = MutableLiveData<Event<Photo>>()
+    val navigateToPhoto: LiveData<Event<Photo>> get() = _navigateToPhoto
+
+
+    private fun refresh(tag : String) {
+        launch {
+            _photos.value = UiModel.Loading
+            _photos.value = UiModel.Content(getPhotos.invoke(tag))
+        }
+    }
+
+
 }
