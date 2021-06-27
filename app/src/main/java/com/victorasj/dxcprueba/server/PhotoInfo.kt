bced364dc6aa3768af096ce2a422ae001842af29
Victor <victor.sanchezjimenez.smr@gmail.com>
2021-06-27 17:42:33 +0200

    Refactorizacion entidades

--- app/src/main/java/com/victorasj/dxcprueba/server/PhotoInfo.kt
+++ app/src/main/java/com/victorasj/dxcprueba/server/PhotoInfo.kt
@@ -1,11 +1,16 @@
 package com.victorasj.dxcprueba.server
 
+import com.google.gson.annotations.SerializedName
+
 class PhotoInfo (
     val id : String,
     val secret : String,
     val server : String,
     val farm : Int,
+    @SerializedName("owner")
     val photoOwner : Owner,
+    @SerializedName("title")
     val photoTitle: Title,
+    @SerializedName("description")
     val photoDescription: Description
 )
