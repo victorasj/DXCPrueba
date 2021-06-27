64a015241239586514fa74156aba85b9d3311306
Victor <victor.sanchezjimenez.smr@gmail.com>
2021-06-27 16:31:01 +0200

    Creacion entidad domain

---
+++ domain/src/main/java/com/victorasj/domain/Photo.kt
@@ -0,0 +1,10 @@
+package com.victorasj.domain
+
+data class Photo (
+    val id : String,
+    val author : String,
+    val title : String,
+    val date : String,
+    val description : String,
+    val image : String
+)
