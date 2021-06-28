1ccff293dc947fcc6d2dbd135867903acdacd1e1
Victor <victor.sanchezjimenez.smr@gmail.com>
2021-06-28 07:11:33 +0200

    Mapeo entidad Flickr API a entidad de dominio para presentación

---
+++ app/src/main/java/com/victorasj/dxcprueba/DataMapper.kt
@@ -0,0 +1,16 @@
+package com.victorasj.dxcprueba
+
+import com.victorasj.domain.Photo
+import com.victorasj.dxcprueba.server.PhotoInfo
+import com.victorasj.dxcprueba.server.PhotoList
+
+
+fun PhotoInfo.toPhoto() : Photo =
+    Photo(
+        id,
+        photoOwner.realname,
+        photoTitle.title,
+        photoDate.taken,
+        photoDescription.description,
+        secret
+    )
