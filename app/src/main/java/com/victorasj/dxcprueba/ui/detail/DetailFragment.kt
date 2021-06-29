2c4de6869e1d0cf704a74458040e388bcf683d54
Victor <victor.sanchezjimenez.smr@gmail.com>
2021-06-29 07:13:19 +0200

    DetailFragment finalizado

--- app/src/main/java/com/victorasj/dxcprueba/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/victorasj/dxcprueba/ui/detail/DetailFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.navArgs
 import com.victorasj.dxcprueba.databinding.DetailFragmentBinding
+import loadUrl
 import org.koin.androidx.scope.ScopeFragment
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.core.parameter.parametersOf
@@ -36,6 +37,11 @@ class DetailFragment : ScopeFragment() {
         when(model){
             is DetailViewModel.UiModel.ContentPhoto -> {
                 with(binding){
+                    textViewTitle.text = model.photo.title
+                    textViewAuthor.text = model.photo.author
+                    textViewDate.text = model.photo.date
+                    textViewDescription.text = model.photo.description
+                    model.photo.image?.let { imageViewPhoto.loadUrl(it) }
                 }
             }
         }
